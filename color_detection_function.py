96e28602723009e9989739e4bafa9ed8e31033cb
Giulia <giuliam.leto@gmail.com>
2020-03-14 22:56:58 +0100

    color detection attempt

---
+++ color_detection_function.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 15:51:05 2020
+
+@author: Giulia
+"""
+
+import cv2;
+import matplotlib.pyplot as plt
+import numpy as np
+
+def filter_color(image_name = 'DelFly_tulip.jpg', y_low = 50, y_high = 200, \
+                 u_low = 120, u_high = 130, v_low = 120, v_high = 130, resize_factor=1):
+    im = cv2.imread(image_name);
+    im = cv2.resize(im, (int(im.shape[1]/resize_factor), int(im.shape[0]/resize_factor)));
+    YUV = cv2.cvtColor(im, cv2.COLOR_BGR2YUV);
+    Filtered = np.zeros([YUV.shape[0], YUV.shape[1]]);
+    for y in range(YUV.shape[0]):
+        for x in range(YUV.shape[1]):
+            if(YUV[y,x,0] >= y_low and YUV[y,x,0] <= y_high and \
+               YUV[y,x,1] >= u_low and YUV[y,x,1] <= u_high and \
+               YUV[y,x,2] >= v_low and YUV[y,x,2] <= v_high):
+                Filtered[y,x] = 1;
+    
+    # plt.figure();
+    # RGB = cv2.cvtColor(im, cv2.COLOR_BGR2RGB);
+    # plt.imshow(RGB);
+    # plt.title('Original image');
+    
+    plt.figure()
+    plt.imshow(Filtered);
+    plt.title('Filtered image');
---
+++ time_to_contact.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 16:13:35 2020
+
+@author: Giulia
+"""
+
+import os
+#from os.path import join
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+import time
+import color_detection_function
+
+#folder_path = "AE4317_2019_datasets/sim_poles/20190121-160844/"
+folder_path = "AE4317_2019_datasets/sim_poles_panels/20190121-161422/"
+#folder_path = "AE4317_2019_datasets/sim_poles_panels_mats/20190121-161931/"
+#folder_path = "20200310-142119/"
+
+
+#Find out how many figures are in the sequence
+path, dirs, files = next(os.walk( folder_path ))
+
+#Inizialize empty array for images
+images = np.empty(len( files ), dtype=object)
+
+#Load all images in the correct sequence and convert colors
+i= 0
+for image in sorted(os.listdir(folder_path)):
+    images[i] = cv2.imread( folder_path+image )
+    #images[i] = cv2.cvtColor(images[i], cv2.COLOR_RGB2BGR)
+    images[i] = cv2.cvtColor(images[i], cv2.COLOR_RGB2YUV)
+    color_detection_function.filter_color(folder_path+image,  y_low = 50, y_high = 200, \
+                      u_low = 0, u_high = 120, v_low = 0, v_high = 135, resize_factor=1)
+    i= i+1
+
+##Show sequence of images
+#for i in range(0, len(files))
+#    plt.imshow(images[i])
+#    pl.pause(.0001)
+
+# #Show single of images
+# plt.imshow(images[0])
+# pl.pause(.1)
+# ##Show single of images
+# plt.imshow(images[70])
